8c9ac5f24bfb173edd5775bd1b773366389e4157
Roman <125725530+RomanGleizer@users.noreply.github.com>
2023-09-05 11:44:28 +0500

    Add operations in history

--- Assets/Scripts/Balance/BalanceHandler.cs
+++ Assets/Scripts/Balance/BalanceHandler.cs
@@ -13,8 +13,20 @@ public class BalanceHandler : MonoBehaviour
 
     public void DoOperation()
     {
-        _operationsCreator.CreateOperation();
-        _balance -= _operationsCreator.Operations.LastOrDefault().Key.Value;
+        _operationsCreator.CreatePurchaseOperation();
+        var operation = _operationsCreator.Operations.LastOrDefault();
+        if (operation == null) return;
+
+        var sum = operation.Value;
+        switch (operation.OperationType)
+        {
+            case OperationType.Purchase: _balance -= sum;
+                break;
+            case OperationType.Add: _balance += sum;
+                break;
+        }
+
+        _operationsCreator.AddOperationInHistory();
         _balanceText.text = _balance.ToString() + " RUB";
     }
 }
--- Assets/Scripts/Operations/OperationsCreator.cs
+++ Assets/Scripts/Operations/OperationsCreator.cs
@@ -1,6 +1,8 @@
 using UnityEngine;
+using UnityEngine.UI;
 using TMPro;
 using System.Collections.Generic;
+using System.Collections;
 
 public class OperationsCreator : MonoBehaviour
 {
@@ -13,25 +15,54 @@ public class OperationsCreator : MonoBehaviour
     [SerializeField] private GameObject _mainMenu;
     [SerializeField] private GameObject _addOperationMenu;
 
-    private Dictionary<Operation, bool> _operations;
-    private MenuOpener _opener;
+    [Header("History of operations")]
+    [SerializeField] private RectTransform[] _history;
 
-    public IDictionary<Operation, bool> Operations => _operations;
+    private List<Operation> _operations;
+    private MenuOpener _opener;
+    private Operation _currentOperation;
+    private int _operationsInHistoryCount;
+
+    public List<Operation> Operations => _operations;
+
+    public TMP_Dropdown OperationType => _operationType;
 
     private void Start()
     {
-        _operations = new Dictionary<Operation, bool>();
+        _operations = new List<Operation>();
         _opener = GetComponent<MenuOpener>();
     }
 
-    public void CreateOperation()
+    public void CreatePurchaseOperation()
     {
         if (!int.TryParse(_sum.text, out _)) return;
 
         var operation = new Operation(int.Parse(_sum.text), (OperationType)_operationType.value);
-        _operations[operation] = false;
+        _sum.text = string.Empty;
+        _currentOperation = operation;
+        _operations.Add(operation);
 
         _opener.OpenMenu(_mainMenu);
         _opener.CloseMenu(_addOperationMenu);
+    }
+
+    public void AddOperationInHistory()
+    {
+        if (_operationsInHistoryCount == 3) _operationsInHistoryCount = 0;
+        var operationType = _history[_operationsInHistoryCount].GetChild(1);
+        var operationSum = _history[_operationsInHistoryCount].GetChild(2);
+
+        if (operationType.TryGetComponent(out TMP_Text type))
+        {
+            print(_operationType.value);
+
+            var text = _operationType.options[_operationType.value].text + $" ({_categories.options[_categories.value].text})";
+            type.text = text;
+        }
+
+        if (operationSum.TryGetComponent(out TMP_Text sum))
+            sum.text = _currentOperation.Value.ToString() + " RUB";
+
+        _operationsInHistoryCount++;
     }
 }
